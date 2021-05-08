b095253b567b22bff0234c41ea865bddd75377c2
Aleksandr Larochkin <infernalred13@gmail.com>
2021-05-08 14:29:18 +0300

    give me diamond

--- Give me a Diamond/Give me a Diamond/Program.cs
+++ Give me a Diamond/Give me a Diamond/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace Give_me_a_Diamond
 {
@@ -14,12 +16,22 @@ namespace Give_me_a_Diamond
     {
         public static string print(int n)
         {
+            if (n < 1)
+                return null;
+            
             if (n % 2 == 0)
                 return null;
 
+            var result = new StringBuilder();
             var middle = n / 2 + 1;
 
-            return null;
+            for (int i = 1; i <= n; i++)
+            {
+                var j = i <= middle ? i : n - i + 1;
+                result.Append(string.Concat(Enumerable.Repeat(" ", middle - j)) + string.Concat(Enumerable.Repeat("*", 2 * j - 1)) + "\n");
+            }
+
+            return result.ToString();
         }
     }
 }
---
+++ Greed is Good/Greed is Good/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Greed_is_Good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public static class Kata
+    {
+        public static int Score(int[] dice) 
+        {
+            // Fill me in!
+        }
+    }
+}
