1fc928491cf2a4796b4ac6dc896e7a48f000cea9
Aleksandr Larochkin <infernalred13@gmail.com>
2021-05-13 23:00:52 +0300

    greed is good

--- Greed is Good/Greed is Good/Program.cs
+++ Greed is Good/Greed is Good/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Greed_is_Good
 {
@@ -12,9 +13,46 @@ namespace Greed_is_Good
     
     public static class Kata
     {
-        public static int Score(int[] dice) 
+        public static int Score(int[] dice)
         {
-            // Fill me in!
+            var dices = new Dictionary<int, string>
+            {
+                {1, ""}, {2, ""}, {3, ""}, {4, ""}, {5, ""}, {6, ""}
+            };
+            
+            Console.WriteLine($"{string.Join(", ",dice)}");
+            
+            var scores = new Dictionary<string, int>
+            {
+                {"1", 100}, {"11", 200},{"111", 1000}, {"1111", 1100},{"222", 200}, {"5", 50}, {"333", 300}, {"444", 400}, {"555", 500}, {"666", 600}
+            };
+
+            foreach (var d in dice)
+            {
+                if (d == 1 || d == 5 || dices[d].Length < 3)
+                    dices[d] += d;
+            }
+
+            var total = 0;
+
+            foreach (var value in dices.Values)
+            {
+                scores.TryGetValue(value, out var score);
+                total += score;
+            }
+
+            return total;
         }
+        
+        //better
+        // int[] tripleValue =  {0, 1000, 200, 300, 400, 500, 600};
+        // int[] singleValue =  {0, 100, 0, 0, 0, 50, 0};
+        //
+        // int value = 0;
+        //     for (int dieSide = 1; dieSide <= 6; dieSide++)
+        // {
+        //     int countRolls = dice.Where(outcome => outcome == dieSide).Count();
+        //     value += tripleValue[dieSide] * (countRolls / 3) + singleValue[dieSide] * (countRolls % 3);                
+        // }
     }
 }
